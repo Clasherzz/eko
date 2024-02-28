5876c2eca52a8811c8f669719e5ae48df322675a
Clasherzz <govindkm20044@gmail.com>
2024-02-28 11:41:23 +0530

    making an endpoint for receiving data from user

--- server.js
+++ server.js
@@ -13,7 +13,50 @@ app.use(bodyParser.json());
 const admin = require('firebase-admin');
 
 // Initialize Firebase Admin SDK
-const serviceAccount = require('./greenbiz-11173-firebase-adminsdk-xajsm-b03e2157d9.json'); // Replace with your service account key
+const serviceAccount = require('./firebase.json'); // Replace with your service account key
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
+});
+
+app.post('/generate-text', async (req, res) => {
+    console.log("hi");
+    const inputString = req.body.input;
+
+    try {
+        const genAI = new GoogleGenerativeAI("AIzaSyCweoRMLwzDJo1VlAgZOFoavZy4jIB9bb4");
+        const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+
+        const prompt = "Write a poem about a starry night";
+        const camera =  await model.generateContent(""+inputString+"");
+        const screen =  await model.generateContent(""+inputString+"");
+        const battery =  await model.generateContent(""+inputString+"");
+        const sensor = await model.generateContent(""+inputString+"");
+        const triggermotor = await model.generateContent(""+inputString+"");
+        const speaker = await model.generateContent(""+inputString+"");
+        
+        
+        
+        const result = await model.generateContent(""+inputString+"");
+        const response = await result.response;
+        const text = response.text();
+        const collectionName = inputString.toLowerCase(); 
+        const collectionRef = admin.firestore().collection(collectionName);
+
+        // Add the generated text to the Firestore collection under the 'description' field
+        await collectionRef.add({
+            camera: camera,
+            screen: screen,
+            battery: battery,
+            sensor: sensor,
+            triggermotor: triggermotor,
+            speaker: speaker,
+        });
+        console.log(text);
+
+        // Send the generated text as a response to the front end
+        res.json({ text });
+    } catch (e) {
+        console.error(e);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
 });
